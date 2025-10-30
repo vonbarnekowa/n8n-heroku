5d2a7d6a079c4c4ea5cce26206831a6cad014245
Alec von Barnekow <von.barnekow.alec@gmail.com>
2025-10-30 11:33:53 +0100

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+npm install jimp
+
 # check if port variable is set or go with default
 if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT="$PORT"; echo "N8N will start on '$PORT'"; fi
 
